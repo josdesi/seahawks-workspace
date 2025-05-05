52d86888d83cbbaa31cf59c7b19275ba25d0be3f
josdesi <josdesi@gmail.com>
2025-05-05 07:12:09 -0600

    chore: clone script

---
+++ clone.sh
@@ -0,0 +1,7 @@
+cd ..
+git clone https://github.com/josdesi/seahawks-web-login.git
+git clone https://github.com/josdesi/seahawks-web-main.git
+git clone https://github.com/josdesi/seahawks-ms-auth.git
+git clone https://github.com/josdesi/seahawks-ms-main.git
+git clone https://github.com/josdesi/seahawks-lambda-warmp.git
+git clone https://github.com/josdesi/seahawks-docs-architecture.git
