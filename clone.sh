f83f997a9c37304fcee1a8cb4b6e1325a2e746d3
josdesi <josdesi@gmail.com>
2025-05-05 07:19:04 -0600

    fix: lambda warmp-up name

--- clone.sh
+++ clone.sh
@@ -3,5 +3,5 @@ git clone https://github.com/josdesi/seahawks-web-login.git
 git clone https://github.com/josdesi/seahawks-web-main.git
 git clone https://github.com/josdesi/seahawks-ms-auth.git
 git clone https://github.com/josdesi/seahawks-ms-main.git
-git clone https://github.com/josdesi/seahawks-lambda-warmp.git
+git clone https://github.com/josdesi/seahawks-lambda-warmp-up.git
 git clone https://github.com/josdesi/seahawks-docs-architecture.git
